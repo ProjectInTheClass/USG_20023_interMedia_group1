1f86d0b68b5c112ce39af63edac04e9552c5d5e3
DAZABAMUKER <wookis112@gmail.com>
2023-02-06 20:50:54 +0900

    get genre

--- USG_2023_intermediate_group1/USG_2023_intermediate_group1/ContentView.swift
+++ USG_2023_intermediate_group1/USG_2023_intermediate_group1/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            getMov()
+                .tabItem {
+                    Image(systemName: "popcorn.fill")
+                    Text("무비 장르 검색")
+                }.tag(0)
         }
-        .padding()
     }
 }
 
