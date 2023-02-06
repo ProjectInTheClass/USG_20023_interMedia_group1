68b775b3a352462fbd75a5e377dac154668b67c3
DAZABAMUKER <wookis112@gmail.com>
2023-02-06 19:50:57 +0900

    project init

---
+++ USG_2023_intermediate_group1/USG_2023_intermediate_group1/USG_2023_intermediate_group1App.swift
@@ -0,0 +1,17 @@
+//
+//  USG_2023_intermediate_group1App.swift
+//  USG_2023_intermediate_group1
+//
+//  Created by 안병욱 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct USG_2023_intermediate_group1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
